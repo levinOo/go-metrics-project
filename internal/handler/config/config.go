0a363a9fb0129c185c026b60832af368273e2eca
Tur <misha99055@gmail.com>
2025-07-25 17:00:28 +0300

    iter9v2

--- internal/handler/config/config.go
+++ internal/handler/config/config.go
@@ -2,8 +2,9 @@ package config
 
 import (
 	"flag"
+	"strconv"
 
-	"github.com/caarlos0/env"
+	"github.com/caarlos0/env/v11"
 )
 
 type Config struct {
@@ -13,17 +14,44 @@ type Config struct {
 	Restore       bool   `env:"RESTORE"`
 }
 
-func GetConfig() (cfg Config, err error) {
-	cfg = Config{}
+func GetConfig() (Config, error) {
+	// 1. Сначала парсим переменные окружения
+	cfg := Config{}
+	if err := env.Parse(&cfg); err != nil {
+		return cfg, err
+	}
 
-	flag.StringVar(&cfg.Addr, "a", "localhost:8080", "address of HTTP server")
-	flag.IntVar(&cfg.StoreInterval, "i", 10, "time interval in seconds for saving data")
-	flag.StringVar(&cfg.FileStorage, "f", "storage.txt", "path to storage file")
-	flag.BoolVar(&cfg.Restore, "r", false, "restore metrics from file on startup")
+	addrFlag := flag.String("a", "localhost:8080", "address of HTTP server")
+	intervalFlag := flag.Int("i", 300, "time interval in seconds for saving data")
+	fileFlag := flag.String("f", "storage.txt", "path to storage file")
+	restoreFlag := flag.String("r", "false", "restore metrics from file on startup (true/false)")
 
 	flag.Parse()
 
-	err = env.Parse(&cfg)
+	if *addrFlag != "" {
+		cfg.Addr = *addrFlag
+	}
+	if *intervalFlag >= 0 {
+		cfg.StoreInterval = *intervalFlag
+	}
+	if *fileFlag != "" {
+		cfg.FileStorage = *fileFlag
+	}
+	if *restoreFlag != "" {
+		if val, err := strconv.ParseBool(*restoreFlag); err == nil {
+			cfg.Restore = val
+		}
+	}
 
-	return cfg, err
+	if cfg.Addr == "" {
+		cfg.Addr = "localhost:8080"
+	}
+	if cfg.StoreInterval == 0 {
+		cfg.StoreInterval = 300
+	}
+	if cfg.FileStorage == "" {
+		cfg.FileStorage = "storage.txt"
+	}
+
+	return cfg, nil
 }
