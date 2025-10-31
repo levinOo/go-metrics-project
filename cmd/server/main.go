0addcca3a29af5378928d58650c48cc8ca65b467
Tur <misha99055@gmail.com>
2025-10-31 11:50:00 +0300

    iter23v1 -Добавил глобальные перменные в cmd/server и cmd/agent для реализации go build -ldflags

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,7 +8,17 @@ import (
 	"github.com/levinOo/go-metrics-project/internal/service"
 )
 
+var (
+	buildVersion string = "N/A"
+	buildDate    string = "N/A"
+	buildCommit  string = "N/A"
+)
+
 func main() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
+
 	if err := run(); err != nil {
 		log.Fatal(err)
 	}
